d0de3597600ef610e5f7529dcef8fd220f08df61
Tabea Riepe <33683714+tabeariepe@users.noreply.github.com>
2023-12-20 13:25:16 +0100

    Update cupcake_chain_samples.sh

--- transcriptomics/sqanti/cupcake_chain_samples.sh
+++ transcriptomics/sqanti/cupcake_chain_samples.sh
@@ -24,10 +24,10 @@ conda deactivate
 
 # SQANTI3 quality control
 singularity run --bind ../../Neural-Retina-Atlas \
-../../../tools/sqanti3_5.1.sif sqanti3_qc.py \
+../../tools/sqanti3_5.1.sif sqanti3_qc.py \
 ${folder}/all_samples.chained.min_fl_2.filtered.gff \
-../../../data/ref_genome/gencode.v39.primary_assembly.annotation.gtf \
-../../../data/ref_genome/hg38.fa \
+../../data/ref_genome/gencode.v39.primary_assembly.annotation.gtf \
+../../data/ref_genome/hg38.fa \
 --CAGE_peak ../../data/ref_genome/human.refTSS_v3.1.hg38.bed \
 --polyA_motif_list ../../data/ref_genome/mouse_and_human.polyA_motif.txt \
 --polyA_peak ../../data/ref_genome/atlas.clusters.2.0.GRCh38.96.bed \
@@ -39,7 +39,7 @@ ${folder}/all_samples.chained.min_fl_2.filtered.gff \
 
 # SQANTI3 rules filter
 singularity run --bind ../../Neural-Retina-Atlas \
-../../../tools/sqanti3_5.1.sif sqanti3_filter.py \
+../../tools/sqanti3_5.1.sif sqanti3_filter.py \
 rules \
 --isoforms ${folder}/all_samples.chained.min_fl_2.filtered_corrected.fasta \
 --gtf ${folder}/all_samples.chained.min_fl_2.filtered_corrected.gtf \
